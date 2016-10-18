88b6a738132906b7b0078e3417df0db935700627
virtmerlin <merlin@sense.net>
2016-10-18 12:01:50 -0500

    [#131763433] config-opsmgr.sh set to create local auth using om tool from eng

--- ci/tasks/ert/config-opsman.sh
+++ ci/tasks/ert/config-opsman.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
 
-exit 1
+sudo cp tool-om/om-linux /usr/local/bin
+
+#Configure Opsman
+om-linux --target https://opsmgr.$pcf_ert_domain -k \
+     configure-authentication \
+       --username "$pcf_opsman_admin" \
+       --password "$pcf_opsman_admin_passwd" \
+       --decryption-passphrase "$pcf_opsman_admin_passwd"
