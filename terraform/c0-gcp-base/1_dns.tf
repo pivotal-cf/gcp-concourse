48eb4958f9d5ca969aa6d8f255916ab639253fbd
virtmerlin <merlin@sense.net>
2016-10-18 10:01:17 -0500

    dns.tf: change opsman dns name from pcf->opsman + .sys_domain

--- terraform/c0-gcp-base/1_dns.tf
+++ terraform/c0-gcp-base/1_dns.tf
@@ -13,7 +13,7 @@ resource "google_dns_managed_zone" "env_dns_zone" {
 ///////////////////////////////////////////////
 
 resource "google_dns_record_set" "ops-manager-dns" {
-  name       = "pcf.${google_dns_managed_zone.env_dns_zone.dns_name}"
+  name       = "opsmgr.${google_dns_managed_zone.env_dns_zone.dns_name}"
   depends_on = ["google_compute_instance.ops-manager"]
   type       = "A"
   ttl        = 300
