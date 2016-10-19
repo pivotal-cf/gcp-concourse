69dc93d14dcb1ed6b196aeac84fbbfcfd294899f
virtmerlin <merlin@sense.net>
2016-10-19 12:53:08 -0500

    fixed opsman link and added dns validation step to c0 script

--- ci/tasks/ert/config-opsman.sh
+++ ci/tasks/ert/config-opsman.sh
@@ -10,7 +10,7 @@ echo "==========================================================================
 
 
 #Configure Opsman
-om-linux --target https://opsmgr.$pcf_ert_domain -k \
+om-linux --target https://opsman.$pcf_ert_domain -k \
      configure-authentication \
        --username "$pcf_opsman_admin" \
        --password "$pcf_opsman_admin_passwd" \
