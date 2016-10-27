7e6d70867e02aeb0ce16c55526e6e014935589df
virtmerlin <merlin@sense.net>
2016-10-27 18:22:47 -0500

    LINKED PIPELINE JOBS

--- ci/tasks/iaas/deploy-director.sh
+++ ci/tasks/iaas/deploy-director.sh
@@ -6,7 +6,11 @@ echo "==========================================================================
 echo "Deploying Director @ https://opsman.$pcf_ert_domain ..."
 echo "=============================================================================================="
 
+# Get jq 1.5
 sudo wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O /usr/bin/jq
 sudo chmod 755 /usr/bin/jq
+
+# Exec bash scripts to config Opsman
+./gcp-concourse/json-opsman/config-director-json.sh
 
 exit 1
