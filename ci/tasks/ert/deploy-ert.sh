3c127794d16350a937ba0edd0b379e1cf19b716f
virtmerlin <merlin@sense.net>
2016-11-01 08:34:23 -0500

    adds: (1) ERT config & deploy scripts to pipeline (2) Apply Changes to Director Config

--- ci/tasks/ert/deploy-ert.sh
+++ ci/tasks/ert/deploy-ert.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
 set -e
 
 
 echo "=============================================================================================="
-echo "Deploying Director @ https://opsman.$pcf_ert_domain ..."
+echo "Deploying ERT @ https://opsman.$pcf_ert_domain ..."
 echo "=============================================================================================="
 
 # Get jq 1.5
@@ -11,11 +10,11 @@ sudo wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O 
 sudo chmod 755 /usr/bin/jq
 
 # Exec bash scripts to config Opsman Director Tile
-./gcp-concourse/json-opsman/config-director-json.sh gcp director
+./gcp-concourse/json-opsman/config-director-json.sh gcp ert
 
 # Apply Changes in Opsman
 
-om-linux --target https://opsman.$pcf_ert_domain -k \
-       --username "$pcf_opsman_admin" \
-       --password "$pcf_opsman_admin_passwd" \
-  apply-changes
+#om-linux --target https://opsman.$pcf_ert_domain -k \
+#       --username "$pcf_opsman_admin" \
+#       --password "$pcf_opsman_admin_passwd" \
+#  apply-changes
