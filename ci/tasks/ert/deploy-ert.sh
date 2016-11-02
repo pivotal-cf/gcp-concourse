7ab806116a12ba0b5401186eee0cc77ef4ad221f
virtmerlin <merlin@sense.net>
2016-11-02 17:39:55 -0500

    Debug missing trust ssl cert flag

--- ci/tasks/ert/deploy-ert.sh
+++ ci/tasks/ert/deploy-ert.sh
@@ -102,7 +102,7 @@ done
 echo "=============================================================================================="
 echo "Applying OpsMan Changes to Deploy: ${guid_cf}"
 echo "=============================================================================================="
-om-linux --target https://opsman.$pcf_ert_domain -k \
-       --username "$pcf_opsman_admin" \
-       --password "$pcf_opsman_admin_passwd" \
-  apply-changes
+#om-linux --target https://opsman.$pcf_ert_domain -k \
+#       --username "$pcf_opsman_admin" \
+#       --password "$pcf_opsman_admin_passwd" \
+#  apply-changes
