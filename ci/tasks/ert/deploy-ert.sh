8caa97ef37f7a65e6e1074dc3b13da6daa6f8b14
virtmerlin <merlin@sense.net>
2016-11-02 16:10:22 -0500

    Pipeline edits to trigger off of new ospman or ert on pivnet

--- ci/tasks/ert/deploy-ert.sh
+++ ci/tasks/ert/deploy-ert.sh
@@ -99,7 +99,9 @@ done
 
 
 # Apply Changes in Opsman
-
+echo "=============================================================================================="
+echo "Applying OpsMan Changes to Deploy: ${guid_cf}"
+echo "=============================================================================================="
 om-linux --target https://opsman.$pcf_ert_domain -k \
        --username "$pcf_opsman_admin" \
        --password "$pcf_opsman_admin_passwd" \
