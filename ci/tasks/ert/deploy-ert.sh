2e23dbf3264a4fe03c71db5c6c17e21246b9a98e
virtmerlin <merlin@sense.net>
2016-11-02 12:14:26 -0500

    test om tool for bulk post to ert config

--- ci/tasks/ert/deploy-ert.sh
+++ ci/tasks/ert/deploy-ert.sh
@@ -1,23 +1,65 @@
 #!/bin/bash
 set -e
 
-echo "=============================================================================================="
-echo "Deploying ERT @ https://opsman.$pcf_ert_domain ..."
-echo "=============================================================================================="
+# Setup OM Tool
+sudo cp tool-om/om-linux /usr/local/bin
+sudo chmod 755 /usr/local/bin/om-linux
 
 # Get jq 1.5
 sudo wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O /usr/bin/jq
 sudo chmod 755 /usr/bin/jq
 
-# Exec bash scripts to config Opsman Director Tile
-./gcp-concourse/json-opsman/config-director-json.sh gcp ert
+echo "=============================================================================================="
+echo "Deploying ERT @ https://opsman.$pcf_ert_domain ..."
+echo "=============================================================================================="
+
+
+function fn_om_linux_curl {
+
+    local curl_method=${1}
+    local curl_path=${2}
+    local curl_data=${3}
+
+     curl_cmd="om-linux --target https://opsman.$pcf_ert_domain -k \
+            --username \"$pcf_opsman_admin\" \
+            --password \"$pcf_opsman_admin_passwd\"  \
+            curl \
+            --request ${curl_method} \
+            --path ${curl_path}"
+
+    if [[ ! -z ${curl_data} ]]; then
+       curl_cmd="${curl_cmd} \
+            --data '${curl_data}'"
+    fi
+
+    echo ${curl_cmd} > /tmp/rqst_cmd.log
+    exec_out=$(((eval $curl_cmd | tee /tmp/rqst_stdout.log) 3>&1 1>&2 2>&3 | tee /tmp/rqst_stderr.log) &>/dev/null)
+
+    if [[ $(cat /tmp/rqst_stderr.log | grep "Status:" | awk '{print$2}') != "200" ]]; then
+      echo "Error Call Failed ...."
+      echo $(cat /tmp/rqst_stderr.log)
+      #exit 1
+    else
+      echo $(cat /tmp/rqst_stdout.log)
+    fi
+}
+
+# Get cf Product Guid
+guid_cf=$(fn_om_linux_curl "GET" "/api/v0/staged/products" \
+            | jq '.[] | select(.type == "cf") | .guid' | tr -d '"' | grep "cf-.*")
 
 
 exit 1
 
-# Apply Changes in Opsman
+# Assign Networks & AZs
+#fn_om_linux_curl "PUT" "/api/v0/staged/products/${guid_cf}/networks_and_azs" "${my_json}"
+
+#list properties
+#fn_om_linux_curl "GET" "/api/v0/staged/products/${guid_cf}/properties"
 
 #om-linux --target https://opsman.$pcf_ert_domain -k \
 #       --username "$pcf_opsman_admin" \
 #       --password "$pcf_opsman_admin_passwd" \
-#  apply-changes
+#  curl \
+#       --path /api/v0/staged/products \
+#       --request GET
