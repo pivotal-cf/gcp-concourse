9b5a81600ad89caf0dc2676c2f7a3660d213a534
virtmerlin <merlin@sense.net>
2016-11-08 10:38:17 -0600

    set dns to cfapps for default apps domain

--- terraform/c0-gcp-base/1_dns.tf
+++ terraform/c0-gcp-base/1_dns.tf
@@ -35,7 +35,7 @@ resource "google_dns_record_set" "wildcard-sys-dns" {
 }
 
 resource "google_dns_record_set" "wildcard-apps-dns" {
-  name       = "*.apps.${google_dns_managed_zone.env_dns_zone.dns_name}"
+  name       = "*.cfapps.${google_dns_managed_zone.env_dns_zone.dns_name}"
   depends_on = ["google_compute_global_address.pcf"]
   type       = "A"
   ttl        = 300
