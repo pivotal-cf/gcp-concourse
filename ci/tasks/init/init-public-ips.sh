c0c3b6a31219318f8abea0310ae788b5371218d5
virtmerlin <merlin@sense.net>
2016-11-09 08:56:33 -0600

    Added print output of DNS entries to init-public-ip.sh

--- ci/tasks/init/init-public-ips.sh
+++ ci/tasks/init/init-public-ips.sh
@@ -20,4 +20,40 @@ echo $gcp_svc_acct_key > /tmp/svc-acct.json
   -var "gcp_terraform_prefix=$gcp_terraform_prefix" \
   gcp-concourse/terraform/$gcp_pcf_terraform_template/init
 
-echo "You have now deployed Public IPs to GCP that must be resolvable to"
+echo "=============================================================================================="
+echo "This gcp_pcf_terraform_template has an 'Init' set of terraform that has pre-created IPs..."
+echo "=============================================================================================="
+echo $gcp_svc_acct_key > /tmp/blah
+gcloud auth activate-service-account --key-file /tmp/blah
+rm -rf /tmp/blah
+
+gcloud config set project $gcp_proj_id
+gcloud config set compute/region $gcp_region
+
+function fn_get_ip {
+     gcp_cmd="gcloud compute addresses list  --format json | jq '.[] | select (.name == \"$gcp_terraform_prefix-$1\") | .address '"
+     api_ip=$(eval $gcp_cmd | tr -d '"')
+     echo $api_ip
+}
+
+pub_ip_global_pcf=$(fn_get_ip "global-pcf")
+pub_ip_ssh_tcp_lb=$(fn_get_ip "tcp-lb")
+pub_ip_ssh_and_doppler=$(fn_get_ip "ssh-and-doppler")
+pub_ip_jumpbox=$(fn_get_ip "jumpbox")
+pub_ip_opsman=$(fn_get_ip "opsman")
+
+echo "You have now deployed Public IPs to GCP that must be resolvable to:"
+echo ""
+read -r -d '' dns_map << EOM
+*.sys.${pcf_ert_domain} == ${pub_ip_global_pcf} \r\n
+*.cfapps.${pcf_ert_domain} == ${pub_ip_global_pcf}
+ssh.sys.${pcf_ert_domain} == ${pub_ip_ssh_and_doppler}
+doppler.sys.${pcf_ert_domain} == ${pub_ip_ssh_and_doppler}
+loggregator.sys.${pcf_ert_domain} == ${pub_ip_ssh_and_doppler}
+tcp.${pcf_ert_domain} == ${pub_ip_ssh_tcp_lb}
+opsman.${pcf_ert_domain} == ${pub_ip_opsman}
+EOM
+
+printf %s "${dns_map[@]}"
+echo ""
+echo "DO Not Start the 'deploy-iaas' Concourse Job of this Pipeline until you have confirmed that DNS is reolving correctly.  Failure to do so will result in a FAIL!!!!"
