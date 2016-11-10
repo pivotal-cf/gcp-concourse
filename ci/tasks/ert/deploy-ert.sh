0c015c6cf9853c0fc2a4b96af47040425a80db6b
virtmerlin <merlin@sense.net>
2016-11-10 10:49:05 -0600

    Upddated ert templates with cloud sql to reduce mysql instances to 0 & removed exit from deploy-ert task to test full deploy via concourse

--- ci/tasks/ert/deploy-ert.sh
+++ ci/tasks/ert/deploy-ert.sh
@@ -143,13 +143,12 @@ for job in $(echo ${json_jobs_configs} | jq . | jq 'keys' | jq .[] | tr -d '"');
 
 done
 
-exit 1
 
 # Apply Changes in Opsman
-#echo "=============================================================================================="
-#echo "Applying OpsMan Changes to Deploy: ${guid_cf}"
-#echo "=============================================================================================="
-#om-linux --target https://opsman.$pcf_ert_domain -k \
-#       --username "$pcf_opsman_admin" \
-#       --password "$pcf_opsman_admin_passwd" \
-#  apply-changes
+echo "=============================================================================================="
+echo "Applying OpsMan Changes to Deploy: ${guid_cf}"
+echo "=============================================================================================="
+om-linux --target https://opsman.$pcf_ert_domain -k \
+       --username "$pcf_opsman_admin" \
+       --password "$pcf_opsman_admin_passwd" \
+  apply-changes
