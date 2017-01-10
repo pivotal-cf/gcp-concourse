6455fabd1a4f7a62f157523b1c75e3b1af85823c
Merlin Glynn <mglynn@pivotal.io>
2017-01-10 14:56:47 -0800

    PDF changed string value od Opsman for GCP,  updated script to grab from pivnet

--- ci/tasks/iaas/upload-opsman.sh
+++ ci/tasks/iaas/upload-opsman.sh
@@ -17,8 +17,8 @@ echo "Getting GCP Ops Manager Image ...."
 echo "=============================================================================================="
 
 /usr/bin/pdftotext pivnet-opsmgr/*GCP*.pdf /tmp/opsman.txt
-opsman_region="US"
-pcf_opsman_image_tgz=$(grep -i -A 1 "$opsman_region:" /tmp/opsman.txt | grep release)
+opsman_region="us"
+pcf_opsman_image_tgz=$(grep -i "ops-manager-${opsman_region}/pcf-gcp-" /tmp/opsman.txt)
 echo "Found GCP OpsMan Image @ $pcf_opsman_image_tgz ...."
 pcf_opsman_image_name="opsman-"$(echo $pcf_opsman_image_tgz | awk -F "/" '{print$2}' | awk -F '.tar.gz' '{print$1}' | tr '.' "-")
 
