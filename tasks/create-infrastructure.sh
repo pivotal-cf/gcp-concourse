e379233e472e609424eb5f1833b7091e8217b3b6
Kris Hicks <khicks@pivotal.io>
2017-01-30 14:25:57 -0800

    create-public-ips -> create-infrastructure

---
+++ tasks/create-infrastructure.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -e
+
+root=$PWD
+version=$(cat tfstate-version/version)
+
+# us: ops-manager-us/pcf-gcp-1.9.2.tar.gz -> ops-manager-us/pcf-gcp-1.9.2.tar.gz
+pcf_opsman_bucket_path=$(grep -i 'us:.*.tar.gz' pivnet-opsmgr/*GCP.yml | cut -d' ' -f2)
+
+# ops-manager-us/pcf-gcp-1.9.2.tar.gz -> opsman-pcf-gcp-1-9-2
+pcf_opsman_image_name=$(echo $pcf_opsman_bucket_path | sed 's%.*/\(.*\).tar.gz%opsman-\1%' | sed 's/\./-/g')
+
+ert_sql_instance_name="${RESOURCE_PREFIX}-sql-$(cat /proc/sys/kernel/random/uuid)"
+
+pcf_ert_ssl_cert=$PCF_ERT_SSL_CERT
+pcf_ert_ssl_key=$PCF_ERT_SSL_KEY
+
+if [[ ${PCF_ERT_SSL_CERT} == "" ]]; then
+  echo "Generating Self Signed Certs for sys.${pcf_ert_domain} & cfapps.${PCF_ERT_DOMAIN} ..."
+  ert-concourse/scripts/ssl/gen_ssl_certs.sh "sys.${pcf_ert_domain}" "cfapps.${pcf_ert_domain}"
+  pcf_ert_ssl_cert=$(cat sys.${pcf_ert_domain}.crt)
+  pcf_ert_ssl_key=$(cat sys.${pcf_ert_domain}.key)
+fi
+
+/opt/terraform/terraform plan \
+  -var "gcp_proj_id=${GOOGLE_PROJECT}" \
+  -var "gcp_region=${GOOGLE_REGION}" \
+  -var "gcp_zone_1=${GCP_ZONE_1}" \
+  -var "gcp_zone_2=${GCP_ZONE_2}" \
+  -var "gcp_zone_3=${GCP_ZONE_3}" \
+  -var "prefix=${RESOURCE_PREFIX}" \
+  -var "pcf_opsman_image_name=${pcf_opsman_image_name}" \
+  -var "pcf_ert_domain=${PCF_ERT_DOMAIN}" \
+  -var "pcf_ert_ssl_cert=${pcf_ert_ssl_cert}" \
+  -var "pcf_ert_ssl_key=${pcf_ert_ssl_key}" \
+  -var "ert_sql_instance_name=${ert_sql_instance_name}" \
+  -var "ert_sql_db_username=${ERT_SQL_DB_USERNAME}" \
+  -var "ert_sql_db_password=${ERT_SQL_DB_PASSWORD}" \
+  -out terraform-$version.tfplan \
+  gcp-concourse/terraform/$gcp_pcf_terraform_template
+
+/opt/terraform/terraform apply \
+  -state-out $root/create-infrastructure-output/terraform-$version.tfstate \
+  terraform-$version.tfplan
+
+echo $GOOGLE_CREDENTIALS > /tmp/blah
+gcloud auth activate-service-account --key-file /tmp/blah
+rm -rf /tmp/blah
+
+gcloud config set project $GOOGLE_PROJECT
+gcloud config set compute/region $GCP_REGION
+
+function fn_get_ip {
+  gcp_cmd="gcloud compute addresses list  --format json | jq '.[] | select (.name == \"${RESOURCE_PREFIX}-${1}\") | .address '"
+  api_ip=$(eval $gcp_cmd | tr -d '"')
+  echo $api_ip
+}
+
+pub_ip_global_pcf=$(fn_get_ip "global-pcf")
+pub_ip_ssh_tcp_lb=$(fn_get_ip "tcp-lb")
+pub_ip_ssh_and_doppler=$(fn_get_ip "ssh-and-doppler")
+pub_ip_jumpbox=$(fn_get_ip "jumpbox")
+pub_ip_opsman=$(fn_get_ip "opsman")
+
+echo "Please configure DNS as follows:"
+echo "----------------------------------------------------------------------------------------------"
+echo "*.sys.${pcf_ert_domain} == ${pub_ip_global_pcf}"
+echo "*.cfapps.${pcf_ert_domain} == ${pub_ip_global_pcf}"
+echo "ssh.sys.${pcf_ert_domain} == ${pub_ip_ssh_and_doppler}"
+echo "doppler.sys.${pcf_ert_domain} == ${pub_ip_ssh_and_doppler}"
+echo "loggregator.sys.${pcf_ert_domain} == ${pub_ip_ssh_and_doppler}"
+echo "tcp.${pcf_ert_domain} == ${pub_ip_ssh_tcp_lb}"
+echo "opsman.${pcf_ert_domain} == ${pub_ip_opsman}"
+echo "----------------------------------------------------------------------------------------------"
--- tasks/wipe-env.sh
+++ tasks/wipe-env.sh
@@ -6,5 +6,18 @@ version=$(cat tfstate-version/version)
 
 /opt/terraform/terraform destroy -force \
   -state $root/tfstate/terraform-*.tfstate \
+  -var "gcp_proj_id=dontcare" \
+  -var "gcp_region=dontcare" \
+  -var "gcp_zone_1=dontcare" \
+  -var "gcp_zone_2=dontcare" \
+  -var "gcp_zone_3=dontcare" \
+  -var "prefix=dontcare" \
+  -var "pcf_opsman_image_name=dontcare" \
+  -var "pcf_ert_domain=dontcare" \
+  -var "pcf_ert_ssl_cert=dontcare" \
+  -var "pcf_ert_ssl_key=dontcare" \
+  -var "ert_sql_instance_name=dontcare" \
+  -var "ert_sql_db_username=dontcare" \
+  -var "ert_sql_db_password=dontcare" \
   -state-out $root/wipe-output/terraform-$version.tfstate \
-  gcp-concourse/terraform/public_ips
+  gcp-concourse/terraform
